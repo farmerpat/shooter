37b1fcdd51cf3097657c6cb98dd10dd0e986f38e
Patrick Connelly <patricksparbanieconnelly@gmail.com>
2018-06-25 15:36:26 -0400

    center a single turrent on top of screen

--- src/Turret.cpp
+++ src/Turret.cpp
@@ -1,7 +1,18 @@
 #include "Turret.h"
 
 void Turret::OnCreate () {
+  // assume that there only one type of turret now...
+  // a single, top-mounted turret...
+  orxFLOAT rot = 180.0 * orxMATH_KF_DEG_TO_RAD;
+  orxVECTOR size;
+  this->GetSize(size);
 
+  // center it
+  //orxVECTOR pos = { 512 + (size.fX / 2), 115, 0 };
+  orxVECTOR pos = { 512 + (size.fX), 115, 0 };
+
+  this->SetRotation(rot, orxFALSE);
+  this->SetPosition(pos);
 }
 
 void Turret::OnDelete () { }
